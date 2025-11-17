4cdfebd8ef02f147ba1d15d66bfd1cc55a08f053
Nathan <G00413711@atu.ie>
2025-11-17 18:19:51 +0000

    V2 navigation with sidebar

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,5 +1,4 @@
 import classes from './MainNavigation.module.css'
-import Link from 'next/link'
 import HamMenu from "../generic/HamMenu"
 import Button from "../generic/Button"
 import Cart from "../generic/Cart"
