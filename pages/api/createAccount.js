7dc1f9b5f68b0696ae179b3a30df965fa5f85de5
Alex Paton <118891272+AlexP8on7@users.noreply.github.com>
2025-12-29 13:56:33 +0000

    Amended API

--- pages/api/createAccount.js
+++ pages/api/createAccount.js
@@ -1,6 +1,6 @@
 async function handler(req, res) {
   if (req.method === "POST") {
-    const apiUrl = process.env.NEXT_PUBLIC_LOGIN_API_URL || "http://localhost:8001";
+    const apiUrl = process.env.LOGIN_API_URL || "http://localhost:8001";
     const response = await fetch(`${apiUrl}/api/login/sign-up`, {
       method: "POST",
       body: JSON.stringify(req.body),
