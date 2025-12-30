c0bc1b2fc80e219736c956aebbdd41ddbff9c9b5
Alex <G00424185@atu.ie>
2025-12-30 11:43:55 +0000

    oder.js API fix

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,7 +1,7 @@
 // /api/new-meetup
 
 async function handler(req, res) {
-  const response = await fetch("http://localhost:8003/api/orders/items");
+  const response = await fetch(`${process.env.NOTIFICATION_API_URL}/api/orders/items`);
 
   const data = await response.json();
   res.status(200).json(data);
