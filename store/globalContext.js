d878d4775726ea7c84d450b52af04bbc72ea2ecf
Alex <G00424185@atu.ie>
2025-12-30 12:47:08 +0000

    Fixed flag for login

--- store/globalContext.js
+++ store/globalContext.js
@@ -11,7 +11,7 @@ export function GlobalContextProvider(props) {
     cartItems: [],
     pastOrders: [],
     dataLoaded: false,
-    isLoggedIn: true,
+    isLoggedIn: false,
     cardDataLoaded: false,
     isPastOrdersLoaded: false,
     userId: 0,
@@ -21,7 +21,7 @@ export function GlobalContextProvider(props) {
  /* const ws = useRef(null);
    
   useEffect(() => {
-    ws.current = new WebSocket("ws://localhost:8080/ws");
+    ws.current = new WebSocket("ws://localhost:8080/ws"); 
     ws.current.onopen = () => console.log("WebSocket connected");
     ws.current.onmessage = (event) => {
       const data = JSON.parse(event.data);
